4147c28227f8811c9fc86b31841e9dd56ffca86d
Karamveer Singh <karamveersingh2003111@Gmail.com>
2024-12-11 09:28:41 +0530

    commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class customer(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class table(models.Model):
+    table_no = models.IntegerField()
+    no_of_seats = models.IntegerField()
+    scanned_time = models.DateTimeField(auto_now_add=True)
+    exited_time = models.DateTimeField(auto_now=True)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'dashboard.html')
+        else:
+            return HttpResponse("Invalid login")
+
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
